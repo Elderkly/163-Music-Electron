3e0dada70fea2e510ca309af6b421b31782df06d
qzp <qzp743@gmail.com>
2019-05-20 12:06:57 +0800

    init project

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: '163-music-electron'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
