0cf7742358e8e9991744114b58e83b19c4ad36f0
qzp <qzp743@gmail.com>
2019-05-20 17:56:36 +0800

    修改窗口配置

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <div class="Top-navigation"></div>
     <router-view></router-view>
   </div>
 </template>
@@ -12,4 +13,13 @@
 
 <style>
   /* CSS */
+  .Top-navigation{
+    position: absolute;
+    width: 100%;
+    top: 0;
+    left: 0;
+    height: 50px;
+    background: rgba(0,0,0,.7);
+    -webkit-app-region: drag
+  }
 </style>
